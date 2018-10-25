1a4ca10462aac9ee6a30b0cd20c35ddb871f78b8
Matt Myers <m@myersmatthew.com>
2018-10-25 12:32:04 -0400

    Fixed bugs with singleton clusters on WSL

--- code/run_pyclone.sh
+++ code/run_pyclone.sh
@@ -8,7 +8,7 @@ samples=`echo $files | xargs -n 1 basename | cut -f 2  -d "."`
 echo $samples
 echo $files
 
-PyClone setup_analysis --in_files $files --samples $samples --working_dir $out_dir/$profile --num_iters 10000 --init_method connected --config_extras_file config/pyclone_config_extras_1.5.yaml
+PyClone setup_analysis --in_files $files --samples $samples --working_dir $out_dir/$profile --num_iters 10000 --init_method connected # --config_extras_file config/pyclone_config_extras_1.5.yaml
 PyClone run_analysis --config_file $out_dir/$profile/config.yaml --seed 1
-PyClone build_table --config_file $out_dir/$profile/config.yaml --out_file $out_dir/$profile/result.txt --table_type loci
+PyClone build_table --config_file $out_dir/$profile/config.yaml --out_file $out_dir/$profile/results.txt --table_type loci
 
